aef5c8e329fd3b63b9137a9c6933f7da993e4764
Curtis Strome <curtis@stro.me>
2025-05-23 13:59:23 -0700

    Use local supabase-js module for Cloudflare compatibility

--- functions/submit.ts
+++ functions/submit.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
 const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY")!;
